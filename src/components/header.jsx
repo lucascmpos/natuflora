af2676bc7831fd65dddb16bcc7112a9a6345e6dd
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-03-26 21:22:05 -0300

    first commit, introducing natu flora store

---
+++ src/components/header.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+
+import NatuFloraLogo from "../assets/natuflora-logo.png";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      if (windowWidth > 900) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
+
+  return (
+    <header className="flex justify-evenly  flex-row w-full  items-center relative">
+      <img className="w-44" src={NatuFloraLogo} />
+      {windowWidth > 767 && (
+        <div className="hidden md:flex flex-row justify-center space-x-9">
+          <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
+            Início
+          </h1>
+
+          <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
+            Sobre
+          </h1>
+          <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
+            Nossos produtos
+          </h1>
+          <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
+            Contato
+          </h1>
+          <div className="flex flex-row gap-5 justify-center pl-20 items-center">
+            <FaInstagram
+              className="text-green-950 hover:text-green-800 transition-all duration-200 cursor-pointer"
+              size={30}
+            />
+            <FaWhatsapp
+              className="text-green-950 hover:text-green-800 transition-all duration-200 cursor-pointer"
+              size={30}
+            />
+          </div>
+        </div>
+      )}
+      <div className="flex gap-8">
+        {windowWidth <= 900 && (
+          <button
+            className="p-3 rounded-lg font-semibold text-green-950 hover:text-green-950 transition duration-300 md:hidden"
+            label={isMenuOpen ? "" : <FaInstagram size={20} />}
+            onClick={toggleMenu}
+          />
+        )}
+      </div>
+      {isMenuOpen && (
+        <>
+          <div
+            className="md:hidden fixed inset-0 bg-black opacity-50 z-10 transition duration-300"
+            onClick={closeMenu}
+          ></div>
+          <div
+            className={`md:hidden fixed inset-0 bg-white z-20 w-64 left-auto transition duration-300 ${
+              isMenuOpen ? "translate-x-0" : "-translate-x-full"
+            }`}
+          >
+            <div className="flex flex-col p-4">
+              <div className="flex justify-between items-center mb-4">
+                <button
+                  className="p-2 rounded-lg font-semibold text-orange-500 hover:text-orange-800 transition duration-300"
+                  label={<FaTimes />}
+                  onClick={toggleMenu}
+                />
+              </div>
+              <div className="flex flex-col gap-8 text-lg text-gray-700 font-semibold cursor-pointer">
+                <h1 className="hover:text-green-950">Entrar</h1>
+                <h1 className="hover:text-green-950">Benefícios</h1>
+                <h1 className="hover:text-green-950">Conta digital</h1>
+                <h1 className="hover:text-green-950">Suporte</h1>
+                <h1 className="hover:text-green-950">Trabalhe Conosco</h1>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/home.jsx
@@ -0,0 +1,48 @@
+import Flower1 from "../assets/flower1.png";
+import BlurFlower from "../assets/about.jpg";
+import { MdOutlineWorkspacePremium, MdLocalShipping } from "react-icons/md";
+import { IoFlower } from "react-icons/io5";
+import { BsShopWindow } from "react-icons/bs";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col gap-52">
+      <div className="flex flex-row mt-16 gap-32 items-center justify-center">
+        <div className="flex flex-col">
+          <h1 className="font-bold text-7xl w-auto text-green-800">
+            As flores que você mais gosta,
+            <br /> da melhor forma!
+          </h1>
+          <h2 className="font-semibold text-3xl text-[#58352d]">
+            Presenteie quem você ama
+          </h2>
+          <button className="bg-green-950 w-52 h-16 mt-5 text-xl text-[#e1e3de] rounded-md">
+            Clique aqui
+          </button>
+        </div>
+        <img className="w-auto " src={Flower1} />
+      </div>
+      <div className="relative z-10">
+        <img className="absolute top-0 left-0" src={BlurFlower} />
+        <div className="relative flex flex-row justify-center items-center mt-64 gap-20 ">
+          <h3 className="text-[#e1e3de] text-2xl flex flex-col justify-center items-center gap-2">
+            <BsShopWindow size={70} />8 anos de história
+          </h3>
+          <h3 className="text-[#e1e3de] text-2xl flex flex-col justify-center items-center gap-2">
+            <MdOutlineWorkspacePremium size={70} />A melhor floricultura mundial
+          </h3>
+          <h3 className="text-[#e1e3de] text-2xl flex flex-col justify-center items-center gap-2">
+            <MdLocalShipping size={70} />
+            Envios para todo Brasil
+          </h3>
+          <h3 className="text-[#e1e3de] text-2xl flex flex-col justify-center items-center gap-2">
+            <IoFlower size={70} />
+            Mais de 30 tipos de flores
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

93682e07bb59c1a377d21b83e8a136add26e9ec0
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-03-28 16:47:05 -0300

    feat: initializing responsiveness

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -33,9 +33,9 @@ const Header = () => {
   }, [windowWidth]);
 
   return (
-    <header className="flex justify-evenly  flex-row w-full  items-center relative">
-      <img className="w-44" src={NatuFloraLogo} />
-      {windowWidth > 767 && (
+    <header className="flex justify-between flex-row w-full items-center relative">
+      <img className="w-44" src={NatuFloraLogo} alt="Natu Flora Logo" />
+      {windowWidth > 767 ? (
         <div className="hidden md:flex flex-row justify-center space-x-9">
           <Link to="/">
             <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
@@ -69,45 +69,46 @@ const Header = () => {
             />
           </div>
         </div>
-      )}
-      <div className="flex gap-8">
-        {windowWidth <= 900 && (
-          <button
-            className="p-3 rounded-lg font-semibold text-green-950 hover:text-green-950 transition duration-300 md:hidden"
-            label={isMenuOpen ? "" : <FaInstagram size={20} />}
-            onClick={toggleMenu}
-          />
-        )}
-      </div>
-      {isMenuOpen && (
-        <>
-          <div
-            className="md:hidden fixed inset-0 bg-black opacity-50 z-10 transition duration-300"
-            onClick={closeMenu}
-          ></div>
-          <div
-            className={`md:hidden fixed inset-0 bg-white z-20 w-64 left-auto transition duration-300 ${
-              isMenuOpen ? "translate-x-0" : "-translate-x-full"
-            }`}
-          >
-            <div className="flex flex-col p-4">
-              <div className="flex justify-between items-center mb-4">
-                <button
-                  className="p-2 rounded-lg font-semibold text-orange-500 hover:text-orange-800 transition duration-300"
-                  label={<FaTimes />}
-                  onClick={toggleMenu}
-                />
-              </div>
-              <div className="flex flex-col gap-8 text-lg text-gray-700 font-semibold cursor-pointer">
-                <h1 className="hover:text-green-950">Entrar</h1>
-                <h1 className="hover:text-green-950">Benefícios</h1>
-                <h1 className="hover:text-green-950">Conta digital</h1>
-                <h1 className="hover:text-green-950">Suporte</h1>
-                <h1 className="hover:text-green-950">Trabalhe Conosco</h1>
-              </div>
+      ) : (
+        <div className="md:hidden">
+          {isMenuOpen ? (
+            <FaTimes
+              className="text-green-950 cursor-pointer"
+              size={30}
+              onClick={toggleMenu}
+            />
+          ) : (
+            <FaBars
+              className="text-green-950 cursor-pointer"
+              size={30}
+              onClick={toggleMenu}
+            />
+          )}
+          {isMenuOpen && (
+            <div className="flex flex-col bg-white absolute top-16 right-0 w-full py-4 px-8 space-y-4">
+              <Link to="/" onClick={closeMenu}>
+                <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
+                  INÍCIO
+                </h1>
+              </Link>
+              <Link to="/about" onClick={closeMenu}>
+                <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
+                  SOBRE
+                </h1>
+              </Link>
+              <Link to="/products" onClick={closeMenu}>
+                <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
+                  PRODUTOS
+                </h1>
+              </Link>
+              <a href="/contato" onClick={closeMenu}>
+                <h1 className="text-green-950 hover:text-green-800 transition-all duration-200 font-semibold text-3xl cursor-pointer">
+                  CONTATO
+                </h1>
+              </a>
             </div>
-          </div>
-        </>
+          )}
+        </div>
       )}
     </header>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,10 +5,10 @@ import BlurFlower2 from "../assets/instagram.png";
 import FlowerSvg from "../assets/flowersvg.svg";
 import Flower2Svg from "../assets/flower2.svg";
 
-import Rosas from "/src/assets/rosas.jpg";
-import Girassol from "/src/assets/girassol.jpg";
-import Lirio from "/src/assets/lirios.jpg";
-import Orquidea from "/src/assets/orquidea.jpg";
+import Rosas from "../assets/rosas.jpg";
+import Girassol from "../assets/girassol.jpg";
+import Lirio from "../assets/lirios.jpg";
+import Orquidea from "../assets/orquidea.jpg";
 
 import Post1 from "../assets/post1.png";
 import Post2 from "../assets/post2.png";
@@ -20,8 +20,8 @@ import PostMobile3 from "../assets/post3mobile.jpg";
 
 import { IoLocationSharp } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
-import { FaPhone } from "react-icons/fa6";
-import { FaWhatsapp } from "react-icons/fa6";
+import { FaPhone } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 import { MdOutlineWorkspacePremium } from "react-icons/md";
 import { RiEmotionHappyFill } from "react-icons/ri";
 import { IoFlower } from "react-icons/io5";
@@ -71,77 +71,76 @@ const Home = () => {
       : [Post1, Post2, Post3];
 
   return (
-    <div className="flex flex-col gap">
+    <div className="flex flex-col">
       <img
         src={FlowerSvg}
         alt="Flower background"
-        className="absolute top-50 z-0 opacity-15"
-        style={{
-          width: "90%",
-          height: "100%",
-          zIndex: -1,
-          fill: "red",
-        }}
+        className="absolute md:top-0 md:left-0 z-0 opacity-15 w-full"
       />
-      <div className="flex flex-row mt-16 gap-32 py-32 items-center justify-center">
-        <div className="flex flex-col">
-          <h1 className="font-bold text-7xl w-auto text-green-800">
+      <div className="flex flex-col md:flex-row mt-16 gap-10 py-32 items-center justify-center">
+        <div className="flex flex-col justify-center items-center text-center md:text-left">
+          <h1 className="font-bold text-4xl md:text-7xl text-green-800">
             As flores que você mais gosta,
             <br /> da melhor forma!
           </h1>
-          <h2 className="font-semibold text-3xl text-[#58352d]">
+          <h2 className="font-semibold text-xl md:text-3xl text-[#58352d]">
             Presenteie quem você ama!
           </h2>
-          <Button style="bg-green-600 hover:bg-green-700 text-[#e1e3de] font-bold w-fit px-2 text-2xl">
+          <Button
+            className="mt-4 md:mt-0"
+            style="bg-green-600 hover:bg-green-700 text-[#e1e3de] font-bold w-fit px-2 text-lg md:text-2xl"
+          >
             ENVIE SEU PRESENTE
           </Button>
         </div>
-        <img className="w-auto " src={Flower1} alt="Flower1" />
+        <img className="w-auto md:w-2/4" src={Flower1} alt="Flower1" />
       </div>
       <div className="relative z-10">
         <div
           className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
           style={{ backgroundImage: `url(${BlurFlower})` }}
         />
-        <div className="relative  flex flex-row justify-center items-center py-32 gap-20 ">
-          <h3 className="text-[#e1e3de] group text-3xl flex flex-col justify-center items-center gap-2">
+        <div className="relative  flex flex-col md:flex-row justify-center items-center py-32 gap-20 ">
+          <h3 className="text-[#e1e3de] group text-xl md:text-3xl flex flex-col justify-center items-center gap-2">
             <BsShopWindow
               className="transition-all duration-200 group-hover:scale-110"
-              size={70}
+              size={40}
             />
             8 anos de história
           </h3>
-          <h3 className="text-[#e1e3de] group text-3xl flex flex-col justify-center items-center gap-2">
+          <h3 className="text-[#e1e3de] group text-xl md:text-3xl flex flex-col justify-center items-center gap-2">
             <MdOutlineWorkspacePremium
               className="transition-all duration-200 group-hover:scale-110"
-              size={70}
+              size={40}
             />
             A melhor floricultura mundial
           </h3>
-          <h3 className="text-[#e1e3de] group text-3xl flex flex-col justify-center items-center gap-2">
+          <h3 className="text-[#e1e3de] group text-xl md:text-3xl flex flex-col justify-center items-center gap-2">
             <RiEmotionHappyFill
               className="transition-all duration-200 group-hover:scale-110"
-              size={70}
+              size={40}
             />
             Atendimento personalizado
           </h3>
-          <h3 className="text-[#e1e3de] group text-3xl flex flex-col justify-center items-center gap-2">
+          <h3 className="text-[#e1e3de] group text-xl md:text-3xl flex flex-col justify-center items-center gap-2">
             <IoFlower
               className="transition-all duration-200 group-hover:scale-110"
-              size={70}
+              size={40}
             />
             Mais de 30 tipos de flores
           </h3>
         </div>
       </div>
-      <div className="flex flex-row py-20 ">
-        <img className="w-2/4" src={Womans} alt="Womans" />
-        <div className="flex flex-col  gap-2 w-1/3 justify-center">
-          <h1 className="text-[#58352d] text-6xl font-semibold">
+      <div className="flex flex-col md:flex-row py-20 ">
+        <img className="w-full md:w-2/4" src={Womans} alt="Womans" />
+        <div className="flex flex-col gap-2 w-full md:w-1/3 justify-center">
+          <h1 className="text-[#58352d] text-3xl md:text-6xl font-semibold">
             A nossa história
           </h1>
-          <h2 className="text-2xl mb-5">Conheça sua floricultura preferida</h2>
-          <p className="text-xl text-left text-gray-700">
+          <h2 className="text-xl md:text-2xl mb-5">
+            Conheça sua floricultura preferida
+          </h2>
+          <p className="text-lg md:text-xl text-left text-gray-700">
             A Natu Flora floresceu com o propósito de oferecer momentos de
             beleza e conexão com a natureza no cotidiano das pessoas. Para os
             amantes de flores e da delicadeza que elas proporcionam, aliada ao
@@ -152,76 +151,66 @@ const Home = () => {
             cada criação.
           </p>
           <Link to="/about" className="text-green-800 hover:underline">
-            <Button style="bg-green-600 hover:bg-green-700 text-[#e1e3de] font-bold w-fit px-2 text-2xl">
+            <Button style="bg-green-600 hover:bg-green-700 text-[#e1e3de] font-bold w-fit px-2 text-lg md:text-2xl">
               LER MAIS
             </Button>
           </Link>
         </div>
-      </div>
-      <div className=" py-20 flex flex-col w-full gap-16 justify-center items-center">
-        <img
-          src={Flower2Svg}
-          alt="Flower background"
-          className="absolute opacity-75 right pl-96 mt-36"
-          style={{
-            width: "90%",
-            height: "100%",
-            zIndex: -1,
-          }}
-        />
-        <h1 className="text-5xl font-semibold text-[#58352d]">
-          Produtos em destaque!
-        </h1>
-        <h2 className="text-gray-700 text-2xl text-center">
-          Desvende nossos produtos e encontre a flor perfeita pro seu momento.
-          <br />
-          Aqui estão as nossas flores mais amadas por vocês!
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-        <Button style="bg-green-600 hover:bg-green-700 text-[#e1e3de] font-bold w-fit px-2 text-2xl">
-          VER TODOS PRODUTOS
-        </Button>
       </div>
       <div className="relative z-10">
         <div
           className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
           style={{ backgroundImage: `url(${BlurFlower2})` }}
         />
-        <div className="relative flex flex-col justify-center items-center py-32 gap-10 ">
-          <h1 className="text-5xl font-semibold text-[#e1e3de]">
-            Fique por dentro das novidades
+        <div className="relative flex flex-col justify-center items-center py-20 md:py-32 gap-10 ">
+          <h1 className="text-5xl md:text-5xl font-semibold text-[#e1e3de]">
+            Produtos em destaque!
           </h1>
-          <h2 className="text-2xl text-gray-200">
-            Nos siga no{" "}
-            <a
-              href="https://www.instagram.com/natu.floraflori/"
-              target="_blank"
-              className="text-pink-800 font-semibold"
-            >
-              instagram
-            </a>
-            !
+          <h2 className="text-lg md:text-2xl text-gray-200 text-center">
+            Desvende nossos produtos e encontre a flor perfeita pro seu momento.
+            <br />
+            Aqui estão as nossas flores mais amadas por vocês!
           </h2>
-          <Carousel className="w-2/3" showStatus={false} showThumbs={false}>
-            {posts.map((post, index) => (
-              <div key={index}>
-                <img src={post} alt={`Post ${index + 1}`} />
-              </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
             ))}
-          </Carousel>
+          </div>
+          <Button style="bg-green-600 hover:bg-green-700 text-[#e1e3de] font-bold w-fit px-2 text-lg md:text-2xl">
+            VER TODOS PRODUTOS
+          </Button>
         </div>
       </div>
+      <div className="relative flex flex-col justify-center items-center py-20 md:py-32 gap-10 ">
+        <h1 className="text-xl md:text-3xl font-semibold text-[#58352d]">
+          Fique por dentro das novidades
+        </h1>
+        <h2 className="text-lg md:text-2xl text-gray-700">
+          Nos siga no{" "}
+          <a
+            href="https://www.instagram.com/natu.floraflori/"
+            target="_blank"
+            className="text-pink-800 font-semibold"
+          >
+            instagram
+          </a>
+          ! Postamos tudo oque acontece por lá!
+        </h2>
+        <Carousel className="w-2/3" showStatus={false} showThumbs={false}>
+          {posts.map((post, index) => (
+            <div key={index}>
+              <img src={post} alt={`Post ${index + 1}`} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
       <div className="bg-[#58352d] flex flex-col justify-center items-center py-20">
-        <h1 className="text-5xl font-semibold text-[#e1e3de] mb-20">
+        <h1 className="text-xl md:text-3xl font-semibold text-[#e1e3de] mb-10">
           Onde estamos
         </h1>
-        <div className="flex flex-row gap-12">
-          <div className="flex flex-col">
-            <h2 className="text-3xl font-semibold text-[#e1e3de]">
+        <div className="flex flex-col md:flex-row gap-12 w-full md:w-auto">
+          <div className="flex flex-col w-full md:w-1/2">
+            <h2 className="text-lg md:text-xl font-semibold text-[#e1e3de]">
               Entre em contato
             </h2>
             <form className="gap-2">
@@ -239,32 +228,32 @@ const Home = () => {
                 placeholder="Digite sua mensagem..."
                 className="w-full py-2 px-4 mb-4 rounded-md bg-[#e1e3de] text-gray-900 h-32 resize-none focus:outline-none focus:ring-2 focus:ring-green-600"
               ></textarea>
-              <Button style="bg-green-600 hover:bg-green-700 text-[#e1e3de] font-bold w-full px-2 text-2xl">
+              <Button style="bg-green-600 hover:bg-green-700 text-[#e1e3de] font-bold w-full px-2 text-lg md:text-2xl">
                 ENVIAR EMAIL
               </Button>
             </form>
           </div>
-          <div id="contato" className="flex flex-col gap-10">
-            <div className="text-2xl flex flex-row gap-2 items-center text-[#e1e3de] mt-4">
+          <div className="flex flex-col gap-10 w-full md:w-1/2">
+            <div className="text-lg md:text-xl flex flex-row gap-2 items-center text-[#e1e3de] mt-4">
               <IoLocationSharp size={40} />
               <div className="flex flex-col">
                 <strong>Endereço:</strong>Rua das Flores, 123, Jardim Botânico,
                 Cidade das Flores, Brasil
               </div>
             </div>
-            <div className="text-2xl flex flex-row gap-2 items-center text-[#e1e3de] mt-4">
+            <div className="text-lg md:text-xl flex flex-row gap-2 items-center text-[#e1e3de] mt-4">
               <MdEmail size={40} />
               <div className="flex flex-col">
                 <strong>Email:</strong>contato@natuflora.com
               </div>
             </div>
-            <div className="text-2xl flex flex-row gap-2 items-center text-[#e1e3de] mt-4">
+            <div className="text-lg md:text-xl flex flex-row gap-2 items-center text-[#e1e3de] mt-4">
               <FaPhone size={40} />
               <div className="flex flex-col">
                 <strong>Telefone:</strong>+55 1234-5678
               </div>
             </div>
-            <div className="text-2xl flex flex-row gap-2 items-center text-[#e1e3de] mt-4">
+            <div className="text-lg md:text-xl flex flex-row gap-2 items-center text-[#e1e3de] mt-4">
               <FaWhatsapp size={40} />
               <div className="flex flex-col">
                 <strong>Whats App</strong>+55 98765-4321
@@ -272,6 +261,12 @@ const Home = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="relative z-10">
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
+          style={{ backgroundImage: `url(${BlurFlower2})` }}
+        />
       </div>
     </div>
   );
